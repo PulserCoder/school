fd3a78db28bee06c6d0ae46cbad98021de7b029a
PulserCoder <92484659+PulserCoder@users.noreply.github.com>
2024-11-23 18:44:22 +0300

    add logging

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import java.util.stream.Collectors;
 @Service
 public class FacultyService {
     private final FacultyRepository facultyRepository;
+    Logger logger = LoggerFactory.getLogger(FacultyService.class);
 
     @Autowired
     public FacultyService(FacultyRepository facultyRepository) {
@@ -20,35 +23,43 @@ public class FacultyService {
     }
 
     public ResponseEntity<Faculty> getFaculty(long id) {
+        logger.info("Get faculty by id: {}", id);
         return ResponseEntity.ok(facultyRepository.findById((int) id).get());
     }
 
     public ResponseEntity<Faculty> addFaculty(Faculty faculty) {
+        logger.info("Add faculty: {}", faculty);
         return ResponseEntity.ok(facultyRepository.save(faculty));
     }
 
     public ResponseEntity<Faculty> updateFaculty(Faculty faculty) {
+        logger.info("Update faculty: {}", faculty);
         return ResponseEntity.ok(facultyRepository.save(faculty));
     }
 
     public void deleteFaculty(long id) {
+        logger.info("Delete faculty by id: {}", id);
         facultyRepository.deleteById((int) id);
     }
 
     public ResponseEntity<List<Faculty>> getFacultyByColor(String color) {
+        logger.info("Get faculty by color: {}", color);
         return ResponseEntity.ok(facultyRepository.findAllByColor(color));
     }
 
     public ResponseEntity<Faculty> findByFacultyByColorOrName(String color, String name) {
+        logger.info("Find faculty by color or name null where color = {} and name = {}", color, name);
         if (color == null) {
             return ResponseEntity.ok(facultyRepository.findFirstByNameIgnoreCase(name));
         } else if (name == null) {
             return ResponseEntity.ok(facultyRepository.findFirstByColorIgnoreCase(color));
         }
+        logger.error("No one faculty with color and name");
         return ResponseEntity.notFound().build();
     }
 
     public ResponseEntity<List<String>> getAllStudents(int id) {
+        logger.info("Get faculty students by id: {}", id);
         return ResponseEntity.ok(facultyRepository.findById(id).get().getStudents().stream().map(e -> e.getName()).toList());
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.controller.StudentController;
@@ -15,7 +17,7 @@ import java.util.Optional;
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
-
+    Logger logger = LoggerFactory.getLogger(StudentService.class);
 
 
     public StudentService(StudentRepository studentRepository) {
@@ -23,45 +25,55 @@ public class StudentService {
     }
 
     public ResponseEntity<Student> getById(long id) {
+        logger.info("Get student by id: {}", id);
         return ResponseEntity.ok(studentRepository.findById((int) id).get());
     }
 
     public void deleteById(long id) {
+        logger.info("Delete student by id: {}", id);
         studentRepository.deleteById((int) id);
     }
 
     public ResponseEntity<Student> update(Student student) {
+        logger.info("Update student by id: {}", student.getId());
         return ResponseEntity.ok(studentRepository.save(student));
     }
 
     public ResponseEntity<Student> create(Student student) {
+        logger.info("Create student by id: {}", student.getId());
         return ResponseEntity.ok(studentRepository.save(student));
     }
 
 
     public ResponseEntity<List<Student>> getByAge(int age) {
+        logger.info("Get students by age: {}", age);
         return ResponseEntity.ok(studentRepository.findAllByAge(age));
     }
 
 
     public ResponseEntity<List<Student>> getByAgeBetween(int min, int max) {
+        logger.info("Get students by age between: {}", min);
         return ResponseEntity.ok(studentRepository.getStudentsByAgeBetween(min, max));
     }
 
     public ResponseEntity<Faculty> getFaculty(int id) {
+        logger.info("Get student faculty by id: {}", id);
         return ResponseEntity.ok(studentRepository.findById(id).get().getFaculty());
     }
 
     public ResponseEntity<Integer> getCountOfStudents() {
+        logger.info("Get student count");
         return ResponseEntity.ok(studentRepository.getCountOfStudents());
     }
 
 
     public ResponseEntity<Float> getAverage() {
+        logger.info("Get student age average");
         return ResponseEntity.ok(studentRepository.getAverage());
     }
 
     public ResponseEntity<List<Student>> getLast5Students() {
+        logger.info("Get student last 5 students");
         return ResponseEntity.ok(studentRepository.getLast5Students());
     }
 }
